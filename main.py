c80722b8ff08984759c4334692fbc3b39f430d47
des9tka <des9tka2005@ukr.net>
2022-11-14 19:18:29 +0200

    hm2

--- main.py
+++ main.py
@@ -47,3 +47,94 @@
 # foo()
 # foo()
 # foo()
+#
+#
+# вивести послідовність Фібоначі, кількість вказана в знінній,
+#   наприклад: x = 10 -> 1 1 2 3 5 8 13 21 34 55
+#   (число з послідовності - це сума попередніх двох чисел)
+# def fibanachi(num: int) -> None:
+#     prev = 0
+#     next = 0
+#     l = []
+#     for i in range(num):
+#         if next == 0:
+#             next = prev + i
+#             l.append(str(next))
+#             prev = i
+#         elif next != 0:
+#             next, prev = prev, next
+#             next = next + prev
+#             l.append(str(next))
+#
+#     print(", ".join(l))
+#
+#
+# порахувати кількість парних і непарних цифр числа,
+#   наприклад: х = 225688 -> п = 5, н = 1;
+#          х = 33294 -> п = 2, н = 3
+# def counter(num: int) -> None:
+#     n = str(num)
+#     p = []
+#     un_p = []
+#     for i in n:
+#         i = int(i)
+#         if i%2 == 1:
+#             un_p.append(i)
+#         else:
+#             p.append(i)
+#
+#     print(f'paired = {len(p)} \nunpaired = {len(un_p)}')
+#
+#
+# прога, що виводить кількість кожного символа з введеної строки,
+#   наприклад:
+#   st = 'as 23 fdfdg544' #введена строка
+#
+#   'a' -> 1  #вивело в консолі
+#   's' -> 1
+#   ' ' -> 2
+#   '2' -> 1
+#   '3' -> 1
+#   'f' -> 2
+#   'd' -> 2
+#   'g' -> 1
+#   '5' -> 1
+#   '4' -> 2
+#
+# def str_count(string: str) -> None:
+#     set = []
+#     for i in string:
+#         if i not in set:
+#             print(f'{i} -> {string.count(i)}')
+#             set.append(i)
+#
+# генерируем лист с непарных чисел в порядке возрастания [1,3,5,7,9.....n]
+# задача сделать c него лист листов такого плана:
+# [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]  => [ [1], [3,5], [7,9,11], [13,15,17,19] ]
+# [1, 3, 5, 7, 9, 11] => [[1], [3, 5], [7, 9, 11]]
+# [1, 3, 5, 7, 9]  => [ [1], [3,5], [7,9]]
+# [1, 3, 5, 7, 9, 11, 13]  => [[1], [3, 5], [7, 9, 11], [13]]
+def l_foo(l: list) -> None:
+    new_list = []
+    items = []
+    index = 1
+    while len(l) != 0:
+        items.append(l[:index])
+        new_list.append(items)
+        items = []
+        index += 1
+    print(new_list)
+
+l_foo([1,3,5,7,9])
+
+# найти со списка только уникальные числа
+# пример [1,2,3,4,2,5,1] => [ 3, 4, 5 ]
+#
+# def uniq_digits(l: list):
+#     new_list = []
+#     for i in l:
+#         item = l.count(i)
+#         if item == 1:
+#             new_list.append(i)
+#
+#     print(new_list)
